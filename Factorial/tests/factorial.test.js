7b4859aac8cc52ac8badfefb76e719b859db362a
Patricia <pirmerpatricia99@gmail.com>
2025-09-23 11:11:18 +0300

    writing the test and moved test to respective folder

---
+++ Factorial/tests/factorial.test.js
@@ -0,0 +1,24 @@
+import { describe, it } from "mocha";
+import { assert } from "chai";
+import factorial from "../factorial.js";
+
+describe('Testing the factorial for 0 and 1', () => {
+  it('return 1 for factorial(0)', () => {
+    assert.equal(factorial(0), 1);
+  })
+  it('return 1 for factorial(1)', () => {
+    assert.equal(factorial(1), 1);
+  })
+})
+
+describe('Testing for smaller numbers', () =>{
+  it('return 2 for factorial of 2', () =>{
+    assert.equal(factorial(2), 2)
+  })
+  it('return 24 for factorial of 4', () =>{
+    assert.equal(factorial(4), 24)
+  })
+  it('return 120 for factorial of 5', () =>{
+    assert.equal(factorial(5), 120)
+  })
+})
--- tests/factorial.test.js
+++
@@ -1,7 +0,0 @@
-import { describe, it } from "mocha";
-import { assert } from "chai";
-import factorial from "../factorial";
-
-describe('Testing the factorial of a number', () => {
-  
-})
