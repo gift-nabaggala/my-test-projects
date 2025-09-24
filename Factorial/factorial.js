56b1544b0575965c67df45cd2b0bc3b790463dfb
gift-nabaggala <nabaggalagift20@gmail.com>
2025-09-24 14:06:01 +0300

    Initial Commit

--- Factorial/factorial.js
+++ Factorial/factorial.js
@@ -1,36 +1,36 @@
 // factorial iterative function
 
 export function factorialIterrative(n) {
-  if (n < 0){
-    return "Not defined for Negative intergers"
+  if (n < 0) {
+    return "Not defined for Negative intergers";
   }
 
-  let result = 1
-  for(let i = 2; i<= n; i++){
-    result *= i
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
   }
-  return result
+  return result;
 }
 
 // factorial recursive function
 export function factorialRecursive(n) {
-  if (n < 0){
-    return "Not defined for Negative intergers"
+  if (n < 0) {
+    return "Not defined for Negative intergers";
   }
-  if (n === 0 || n === 1){
-    return 1
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  return n * factorialRecursive(n - 1)
+  return n * factorialRecursive(n - 1);
 }
 // factorial using formula n! = sqrt(2 * pi * n) * (n / e)^n
 export function factorialFormula(n) {
-  if (n < 0){
-    return "Not defined for Negative intergers"
+  if (n < 0) {
+    return "Not defined for Negative intergers";
   }
-  if (n === 0 || n === 1){
-    return 1
+  if (n === 0 || n === 1) {
+    return 1;
   }
-  const pi = Math.PI
-  const e = Math.E
-  return Math.round(Math.sqrt(2 * pi * n) * Math.pow(n / e, n))
+  const pi = Math.PI;
+  const e = Math.E;
+  return Math.round(Math.sqrt(2 * pi * n) * Math.pow(n / e, n));
 }
